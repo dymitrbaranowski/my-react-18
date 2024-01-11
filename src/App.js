04d9543a023f0e584f0b408f070aee079b94f99c
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-01-11 21:20:53 +0200

    fox3

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 // import Counter from "./components/Counter";
 // import Dropdown from "./components/Dropdown";
-import ColorPicker from "./components/ColorPicker";
+// import ColorPicker from "./components/ColorPicker";
 import TodoList from "./components/TodoList";
-// import initialTodos from "./todos.json";
+import initialTodos from "./todos.json";
 
-const colorPickerOptions = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "grey", color: "#607D8B" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F51B5" },
-];
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "grey", color: "#607D8B" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
 
 class App extends Component {
-  // state = {
-  //   todos: initialTodos,
-  // };
+  state = {
+    todos: initialTodos,
+  };
 
-  // deleteTodo = (todoId) => {
-  //   this.setState((prevState) => ({
-  //     todos: prevState.todos.filter((todo) => todo.id !== todoId),
-  //   }));
-  // };
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
+  };
 
   render() {
-    // const { todos } = this.state;
+    const { todos } = this.state;
 
     // const totalTodoCount = todos.length;
     // const completedTodoCount = todos.reduce(
@@ -42,12 +42,12 @@ class App extends Component {
         {/* <Dropdown /> */}
         {/* <ColorPicker options={colorPickerOptions} /> */}
 
-        {/* <div>
+        <div>
           <p>Общее кол-во: {totalTodoCount}</p>
           <p>Кол-во выполненных: {completedTodoCount}</p>
-        </div> */}
+        </div>
 
-        <TodoList />
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </>
     );
   }
